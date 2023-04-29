513aebb8758ca35e14c84f31f833e6da8e73b168
rogercerpa <roger.e.cerpa@gmail.com>
2023-04-29 12:48:38 -0400

    added react icons, updated the product list styling

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -1,23 +1,23 @@
+import { AiFillDelete } from "react-icons/ai";
 export default function GroceryList(props) {
   const { groceryItem, handleDelete } = props;
 
   const productList = groceryItem.map((item) => {
     return (
-      <li key={item.id}>
-        {item.itemName}
-        <button
-          className="outline outline-offset-2 outline-pink-500 ..."
-          onClick={() => handleDelete(item.id)}
-        >
-          Delete
+      <div className="flex flex-row">
+        <li className="m-1 shadow-md ..." key={item.id}>
+          {item.itemName}
+        </li>
+        <button className="" onClick={() => handleDelete(item.id)}>
+          <AiFillDelete />
         </button>
-      </li>
+      </div>
     );
   });
 
   return (
-    <div>
-      <h1>My list of Products</h1>
+    <div className="">
+      <h1 className="font-bold ...">My list of Products</h1>
       <ul className="list-disc" id="shopping-list">
         {productList}
       </ul>
