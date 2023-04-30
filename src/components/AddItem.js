22f3315fd15d716af43e8b1d726032ca4716969b
rogercerpa <roger.e.cerpa@gmail.com>
2023-04-30 11:21:47 -0400

    updated add to card button, added select element, added requirements

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GroceryList from "./GroceryList";
+import { ImCart } from "react-icons/im";
 
 export default function AddItem(props) {
   return (
@@ -12,9 +13,24 @@ export default function AddItem(props) {
           onChange={props.onChange}
           value={props.itemName}
           name="itemName"
+          required
         ></input>
-        <button className="text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 cursor-pointer py-4 px-8 rounded-md text-lg">
+
+        <select
+          className="box-border border-none focus:border-3 focus:border-gray-500 rounded-md w-full text-2xl text-center my-2"
+          required
+        >
+          <option value="" disabled selected>
+            Select an option
+          </option>
+          <option>Option 1</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+        </select>
+
+        <button className="flex flex-row justify-center items-center text-zinc-950 bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 cursor-pointer py-4 px-8 gap-5 rounded-md text-lg">
           Add to Cart
+          <ImCart />
         </button>
       </form>
       <GroceryList
