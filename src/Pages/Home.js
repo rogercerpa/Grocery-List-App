22d66ee089019aeab62fec29bf35eebfc1d64e13
rogercerpa <roger.e.cerpa@gmail.com>
2023-05-25 21:59:19 -0400

    updated home page styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,14 +5,14 @@ import Image from "../components/Image";
 const Home = (props) => {
   const { user, setUser, auth } = props;
   return (
-    <div className="home">
+    <div className="w-full flex flex-col items-center ">
       <h1>Welcome to GroceryList App</h1>
       {!user && (
         <p>Sign up or sign in to access the main features of the app.</p>
       )}
 
       {user && (
-        <div>
+        <div className="w-full flex flex-col items-center ">
           <h2>Start Creating your grocery list today!</h2>
           <Image />
           <AddItems />
--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,7 @@ import Cart from "../assets/groceryCart.jpg";
 
 export default function Image() {
   return (
-    <div>
+    <div className="flex items-center">
       <img
         src={Cart}
         className="p-5 w-150px h-150px rounded-lg"
