8379cce44bfeb13f51a9d66066ebd9bdbc1f320f
rogercerpa <roger.e.cerpa@gmail.com>
2023-06-07 22:30:02 -0400

    added default user profile picture if user doesnt have one in the database

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -9,6 +9,9 @@ import {
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "../firebaseConfig";
+import UserDefaultImage from "../assets/day22-owl.png";
+// const UserDefaultImage =
+//   "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.winhelponline.com%2Fblog%2Freplace-default-user-account-picture-avatar-windows-10%2F&psig=AOvVaw1yRptsclNf8gfDO-_WEuhL&ust=1686275905665000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIip6bTJsv8CFQAAAAAdAAAAABAE";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -34,6 +37,10 @@ const Profile = ({ user }) => {
       if (docSnap.exists()) {
         setUsername(docSnap.data().username);
         setImageUrl(docSnap.data().imageUrl);
+      } else {
+        // If there's no user data in the database, use the default image
+        setImageUrl(UserDefaultImage);
+        console.log(UserDefaultImage);
       }
     };
 
@@ -54,26 +61,27 @@ const Profile = ({ user }) => {
     event.preventDefault();
 
     try {
-      // 1. Upload the image to Firebase Storage
-      const storageRef = ref(storage, `profile_photos/${user.uid}`);
-      await uploadBytesResumable(storageRef, photo);
+      let url = imageUrl; // start with the current imageUrl
+      if (photo) {
+        // If a new photo was selected, upload it and get the new URL
+        const storageRef = ref(storage, `profile_photos/${user.uid}`);
+        await uploadBytesResumable(storageRef, photo);
+        url = await getDownloadURL(storageRef);
+      }
 
-      // 2. Get the download URL
-      const url = await getDownloadURL(storageRef);
-
-      // 3. Save the new profile info to Firestore
+      // Save the new profile info to Firestore
       await setDoc(doc(db, "users", user.uid), {
         username: username,
         imageUrl: url,
       });
 
-      // 4. Update the profile in Firebase Auth
+      // Update the profile in Firebase Auth
       await updateProfile(auth.currentUser, {
         displayName: username,
         photoURL: url,
       });
 
-      // 5. Toggle editing off
+      // Toggle editing off
       setEditing(false);
     } catch (error) {
       console.error("An error occurred during file upload:", error);
@@ -89,24 +97,21 @@ const Profile = ({ user }) => {
     <main className="bg-gray-200 font-sans h-screen w-full flex flex-row justify-center items-center ">
       {user && (
         <div className="card w-96 mx-auto bg-white  shadow-xl hover:shadow rounded-lg">
-          {imageUrl && (
-            <img
-              className="w-32 mx-auto rounded-full -mt-20 border-8 border-white"
-              src={imageUrl}
-              alt="Profile"
-            />
-          )}
+          {/* {imageUrl && ( */}
+          <img
+            className="w-32 mx-auto rounded-full -mt-20 border-8 border-white"
+            src={UserDefaultImage}
+            alt="ProfileImage"
+          />
+          {/* )} */}
+
           <h1>Profile</h1>
 
           <p className="text-center mt-2 text-3xl font-medium">
-            Username: {username.username}
+            {username.username}
           </p>
-          <p className="text-center mt-2 font-light text-sm">
-            Email: {user.email}
-          </p>
-          <p className="text-center font-normal text-lg">
-            location: {user.location}
-          </p>
+          <p className="text-center mt-2 font-light text-sm">{user.email}</p>
+          <p className="text-center font-normal text-lg">{user.location}</p>
           <p className="px-6 text-center mt-2 font-light text-sm">
             Bio: {user.bio}
           </p>
