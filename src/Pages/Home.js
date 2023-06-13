d54eebe60cda9aa833c2f63cf3decefcbdc1c8be
rogercerpa <roger.e.cerpa@gmail.com>
2023-06-12 20:47:53 -0400

    updated the home page welcome message

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,7 +7,7 @@ const Home = (props) => {
   return (
     <div className="w-full flex flex-col items-center ">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-        Welcome to GroceryList App
+        GroceryList App
       </h1>
       {!user && (
         <p className="mt-6 text-lg leading-8 text-gray-600">
