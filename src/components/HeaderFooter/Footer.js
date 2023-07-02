d820568c1815f8108727f3386a757177e2e88cb5
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-02 16:19:54 -0400

    got the profile button on the footer working

--- src/components/HeaderFooter/Footer.js
+++ src/components/HeaderFooter/Footer.js
@@ -2,6 +2,7 @@ import { HomeIcon } from "@heroicons/react/20/solid";
 import { MagnifyingGlassIcon as SearchIcon } from "@heroicons/react/20/solid";
 import { BellIcon } from "@heroicons/react/20/solid";
 import { UserIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
 
 function Footer() {
   const handleClick = (event) => {
@@ -11,26 +12,30 @@ function Footer() {
 
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-gray-800 p-2 flex items-center justify-around text-white">
-      <HomeIcon
-        className="h-6 w-6 cursor-pointer"
-        name="home"
-        onClick={handleClick}
-      />
-      <SearchIcon
-        className="h-6 w-6 cursor-pointer"
-        name="search"
-        onClick={handleClick}
-      />
-      <BellIcon
-        className="h-6 w-6 cursor-pointer"
-        name="notifications"
-        onClick={handleClick}
-      />
-      <UserIcon
-        className="h-6 w-6 cursor-pointer"
-        name="profile"
-        onClick={handleClick}
-      />
+      <Link>
+        <HomeIcon
+          className="h-6 w-6 cursor-pointer"
+          name="home"
+          onClick={handleClick}
+        />
+      </Link>
+      <Link>
+        <SearchIcon
+          className="h-6 w-6 cursor-pointer"
+          name="search"
+          onClick={handleClick}
+        />
+      </Link>
+      <Link>
+        <BellIcon
+          className="h-6 w-6 cursor-pointer"
+          name="notifications"
+          onClick={handleClick}
+        />
+      </Link>
+      <Link to="/profile">
+        <UserIcon className="h-6 w-6 cursor-pointer" name="profile" />
+      </Link>
     </footer>
   );
 }
