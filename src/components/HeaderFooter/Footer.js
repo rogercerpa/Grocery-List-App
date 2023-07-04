a138ba49dd8eee99d81ed20c7be4f231a964a32f
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-04 18:43:35 -0400

    updated footer padding to fit better on phone screens

--- src/components/HeaderFooter/Footer.js
+++ src/components/HeaderFooter/Footer.js
@@ -6,24 +6,24 @@ import OutdoorGrillIcon from "@mui/icons-material/OutdoorGrill";
 
 function Footer({ displayFavorites }) {
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-gray-800 p-2 flex items-center justify-around text-white">
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-800 p-6 flex items-center justify-around text-white ">
       <Link to="/">
         <HomeIcon
-          className="h-6 w-6 cursor-pointer"
+          className="h-8 w-8 cursor-pointer"
           name="home"
           content="HomePage"
         />
       </Link>
       <Link to="recipes">
         <SearchIcon
-          className="h-6 w-6 cursor-pointer"
+          className="h-8 w-8 cursor-pointer"
           name="search"
           content="SearchRecipes"
         />
       </Link>
       <Link>
         <OutdoorGrillIcon
-          className="h-6 w-6 cursor-pointer"
+          className="h-8 w-8 cursor-pointer"
           name="favoritedRecipes"
           content="FavoritedRecipes"
           onClick={displayFavorites}
@@ -31,7 +31,7 @@ function Footer({ displayFavorites }) {
       </Link>
       <Link to="/profile">
         <UserIcon
-          className="h-6 w-6 cursor-pointer"
+          className="h-8 w-8 cursor-pointer"
           name="profile"
           content="profile"
         />
