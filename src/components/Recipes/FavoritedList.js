6d929309bdea5109012d31837c7bdfaaee34c2e9
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-23 21:31:31 -0400

    updated favorited list styling

--- src/components/Recipes/FavoritedList.js
+++ src/components/Recipes/FavoritedList.js
@@ -7,28 +7,29 @@ const FavoriteList = ({ favoriteRecipes }) => {
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
           <h2 className="text-2xl font-bold text-gray-900">Favorite Recipes</h2>
 
-          <div className="">
+          <div className="grid grid-rows-4 grid-flow-col gap-4">
             {favoriteRecipes.map((recipe) => (
-              <div key={recipe.id} className="group relative">
-                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                  <img
-                    src={recipe.image} // Assuming 'image' is the key for the recipe image URL
-                    alt={recipe.title} // Assuming 'title' is the key for the recipe title
-                    className="h-full w-full object-cover object-center"
-                  />
-                </div>
-                <h3 className="mt-6 text-sm text-gray-500">
+              <div key={recipe.id} className=" ">
+                <h3 className="p-1 text-sm text-gray-500 content-center">
                   <a href={`/recipe/${recipe.id}`}>
                     {" "}
                     {/* Adjust this to match your route for individual recipes */}
-                    <span className="absolute inset-0" />
+                    <span className="" />
                     {recipe.title}
                   </a>
                 </h3>
+                <div className="">
+                  <img
+                    src={recipe.image} 
+                    alt={recipe.title} 
+                    className="h-60 w-60  rounded-lg sm:h-64 sm:w-64 lg:h-80 lg:w-80"
+                  />
+                </div>
+
                 <p className="text-base font-semibold text-gray-900">
                   {recipe.description}
                 </p>{" "}
-                {/* Assuming 'description' is the key for the recipe description */}
+             
               </div>
             ))}
           </div>
