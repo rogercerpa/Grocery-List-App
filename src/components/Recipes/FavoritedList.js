714418dbdf2914c7c3f26e25331c9f349d33f323
rogercerpa <roger.e.cerpa@gmail.com>
2023-08-16 10:11:35 -0400

    updated favorire recipes screen size response

--- src/components/Recipes/FavoritedList.js
+++ src/components/Recipes/FavoritedList.js
@@ -23,9 +23,14 @@ const FavoriteList = ({ favoriteRecipes }) => {
     <div className="">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-          <h2 className="text-2xl font-bold text-gray-900">Favorite Recipes</h2>
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+        Favorite Recipes
+      </h1>
+      <h2 className="mt-6 text-lg leading-8 text-gray-600">
+            Find all your favorite recipes here, start Cooking! 
+      </h2>
 
-          <div className="grid grid-rows-4 grid-flow-col gap-4">
+          <div className="grid grid-rows-4 grid-flow-col gap-2">
             {favoriteRecipes.map((recipe) => (
               <div key={recipe.id} className=" ">
                 <h3 className="p-1 text-sm text-gray-500 content-center">
@@ -36,22 +41,24 @@ const FavoriteList = ({ favoriteRecipes }) => {
                   </Link>
                 </h3>
                 <div className="">
-                  <img
+                <img
                     src={recipe.image} 
                     alt={recipe.title} 
-                    className="h-60 w-60  rounded-lg sm:h-64 sm:w-64 lg:h-80 lg:w-80"
-                  />
+                    className="h-20 w-20 rounded-lg sm:h-40 sm:w-40 md:h-60 md:w-60 lg:h-80 lg:w-80"
+                 />
+
                 </div>
 
                 <p className="text-base font-semibold text-gray-900">
                   {recipe.description}
                 </p>{" "}
+                
                 <button 
-        // onClick={() => deleteFavorite(recipe.id)}
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Delete
-      </button>
+              //  onClick={() => deleteFavorite(recipe.id)}
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                Delete
+                </button>
              
               </div>
             ))}
