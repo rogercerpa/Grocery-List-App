d4c5edc7487c987f9762c33f12175a09f3ded29a
rogercerpa <roger.e.cerpa@gmail.com>
2023-09-11 23:20:08 -0400

    fixed budgettable component styling screen sizes issue

--- src/Pages/Calculator.jsx
+++ src/Pages/Calculator.jsx
@@ -4,14 +4,14 @@ import BudgetTable  from "../components/BudgetTable"
 
 function Calculator() {
   return (
-<div className="w-full max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center ">
-    <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold tracking-tight text-gray-900 my-4">
+<div className="w-full flex flex-col items-center ">
+    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
         Calculate your Grocery cost
     </h1>
-    <h2 className="text-base md:text-lg lg:text-xl leading-8 text-gray-600 my-4">
+    <h2 className="mt-6 text-lg leading-8 text-gray-600">
         Stay within budget! Add all your items as you go.
     </h2>
-    <div className="w-full mt-6">
+    <div >
         <BudgetTable/>
     </div>
 </div>
